b3574bc9cbb16d57690525bcb29c5907982d579d
ixinyu <43402453+ixinyu@users.noreply.github.com>
2020-11-20 12:16:07 +0800

    Update sidebarItem.vue

--- src/Layout/components/sidebarItem.vue
+++ src/Layout/components/sidebarItem.vue
@@ -3,7 +3,7 @@
     <fragment v-if="hasOneShowingChild(item.children,item)&&!item.hidden">
       <router-link :to="resolvePath(onlyOneChild.path)">
         <el-menu-item :index='resolvePath(onlyOneChild.path)'>
-          <i class="el-icon-setting"></i>
+          <i :class="item.meta.icon"></i>
           <span slot="title">{{item.meta.title}} {{item.path}}</span>
         </el-menu-item>
       </router-link>
