7f49d25f7f09fbac0e5448c0a5a23f943a8c53cd
ixinyu <43402453+ixinyu@users.noreply.github.com>
2020-11-21 09:59:21 +0800

    Update sidebarItem.vue

--- src/Layout/components/sidebarItem.vue
+++ src/Layout/components/sidebarItem.vue
@@ -11,7 +11,7 @@
 
     <el-submenu v-else :index='resolvePath(item.path)'>
       <template slot="title">
-        <i class="el-icon-location"></i>
+        <i :class="onlyOneChild.meta.icon||(item.meta&&item.meta.icon)"></i>
         <span>{{item.meta.title}}</span>
       </template>
       <sidebar-item
